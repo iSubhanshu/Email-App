ffd946c312c4f2c23908114bcef7d073299fe2f8
Subhanshu <isubhanshu@gmail.com>
2023-01-04 16:04:49 +0530

    Add to repository

---
+++ src/exceptionsfiles/CommonExceptions.java
@@ -0,0 +1,36 @@
+package exceptionsfiles;
+
+public class CommonExceptions {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 0;
+		
+		// Identify the potential problem area
+		
+		// int c = a/b;
+		
+		// Surround with try-catch block
+		try {
+			int c = a/b;
+		} catch (ArithmeticException e) {
+			System.out.println("CANNOT DIVIDE BY ZERO");
+			System.out.println(e.toString());
+		}
+		
+		
+		String[] states = {"CA", "TX", "FL", "NY"};
+		
+		for (int i=0; i<=states.length; i++){
+			try {
+				System.out.println(states[i]);
+			} catch (ArrayIndexOutOfBoundsException e) {
+				System.out.println("ERROR: Index outsied of array domain");
+				System.out.println(e.toString());
+			} finally {
+				System.out.println("Iterating through array");
+			}
+		}
+		System.out.println("Program is closing");
+	}
+}
---
+++ src/exceptionsfiles/Demo.java
@@ -0,0 +1,12 @@
+package exceptionsfiles;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 0;
+		int c = a/b;
+
+	}
+
+}
